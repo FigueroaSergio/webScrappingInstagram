846a957d20c5c9b670f96218921b24e593ff64f7
Sergio Figueroa <figueroa.sergio.26@gmail.com>
2021-03-23 19:22:55 -0500

    open browser

--- routes/browser.js
+++ routes/browser.js
@@ -1,8 +1,31 @@
 const puppeteer = require('puppeteer');
-async function search(person){
-    const browser = await puppeteer.launch()
+async function search(person) {
+
+    const url = `https://www.instagram.com/${person}`
+    console.log(url)
+    const browser = await puppeteer.launch({headless: false,slowMo:1000})
     const page = await browser.newPage()
-    await page.goto(`https://www.instagram.com/${person}/`)
-    page.screenshot({ path: `./public/images/${person}.png` })
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36');
+
+    await page.goto(url)
+   
+    await page.screenshot({ path: `./public/images/${person}.png` })
+    await page.waitForTimeout(5000)
+
+    let obj = {}
+    
+    await page.waitForSelector("[data-testid=user-avatar]")
+    const imgProfile = await page.evaluate(() => {
+        const img = document.querySelector('[data-testid=user-avatar]').getAttribute("src");
+
+        return img;
+    });
+    obj["Nombre"] = person
+    obj["Imagen"] = imgProfile
+    await browser.close()
+    console.log(obj)
+    return obj
+
 
 }
+module.exports = (person) => search(person)
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var search = require("./browser")
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 router.post('/', (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.send({"tuBusqueda":`${req.body.busqueda}`})  
+  person = req.body.person
+  console.log(person)
+  search(person).then((datos) => { res.json({ "status": datos }) }, (err) => next(err)).catch((err) => {console.log("error")});
+
 });
 
 module.exports = router;
